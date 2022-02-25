b466cfccc8dfce2046fbfec9a25bf9263b0e90a4
HarleyT <harley.trappitt@gmail.com>
2022-02-25 14:13:16 +1100

    Update Bezier Curve Array.js

--- sketches/Bezier Curve Array.js
+++ sketches/Bezier Curve Array.js
@@ -15,21 +15,42 @@ function getVector(index, total) {
 
 function draw() {
   background(0);
-  const total = 200;
-  factor += 0.05;
+  const total = 1000;
+  factor += 0.02;
+  text(frameCount, 10, 20);
 
+  let colArrayCounter = 0;
+  let colArray = [
+  color(25, 165, 190),
+  color(95, 170, 200),
+  color(120, 190, 210),
+  color(170, 210, 230),
+  color(205, 225, 245),
+  color(220, 240, 250)]
+  
   translate(width/2, height/2);
-  stroke(255, 100);
-  strokeWeight(2);
+  //stroke(255, 100);
   noFill();
-
-  strokeWeight(2);
+  //strokeWeight(1);
   for (let i = 0; i < total - 1; i++) {
     const a = getVector(i, total);
     const b = getVector(i + 12, total);
     const c = getVector((i + 8) * factor, total);
     const d = getVector((i + 3) * factor, total);
 
-    bezier(a.x, a.y, b.x, b.y, c.x, c.y, d.x, d.y);
+    colArrayCounter++;
+    if(colArrayCounter>5) colArrayCounter = 0;
+    stroke(colArray[colArrayCounter]);
+    
+    strokeWeight(0.5);
+    //bezier(a.x, a.y, b.x, b.y, c.x, c.y, d.x, d.y);
+    line(d.x/2, c.y, a.x, a.y);
     }
   }
+function mousePressed() {
+  noLoop();
+}
+
+function mouseReleased() {
+  loop();
+}
